5e17102adbd1965458e07c0fb592eeee905a89e3
Utkarsha Kshirsagar <utkarshakshirsagar4717@gmail.com>
2024-08-08 18:50:16 -0400

    first commit

---
+++ src/components/MarkdownUseState.js
@@ -0,0 +1,9 @@
+function MarkdownUseState (){
+    return (
+        <div className="container">
+           <textarea className="textarea" placeholder="Enter markdown"></textarea>
+           <div className="preview">Hello </div>
+        </div>
+    )
+}
+export default   MarkdownUseState ;
