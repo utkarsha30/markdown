9c9b3ee04643dfed812c7b6b2372c8402eb6bf94
Utkarsha Kshirsagar <utkarshakshirsagar4717@gmail.com>
2024-08-13 18:59:32 -0400

    completed the code for parent and child

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 // import MarkdownUseState from './components/MarkdownUseState';
 import Parent from "./components/ParentContainer";
+import Child from "./components/ChildContainer"
+import React, { useState } from "react";
 import   "./styles.css"
 import './App.css';
 
 
 function App() {
+  const [cartItems,setCartItems] = useState([]);
+  
+  const removeItem=(index)=>{
+    console.log(cartItems.filter((item,i)=>i!==index));
+    setCartItems(cartItems.filter((item,i)=>i!==index))
+  }
   return (
-    <div >
+    <div  class="app-container">
     {/* <MarkdownUseState/> */}
-    <Parent/>
+    <Parent setCartItems={setCartItems}  />
+    <Child cartItems = {cartItems} removeItem={removeItem}/>
 
     </div>
   );
